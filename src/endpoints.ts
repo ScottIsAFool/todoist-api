9e3232dccde3a5ab8c2a58a72fc1a1eed58944f9
Scott Lovegrove <scottisafool@live.co.uk>
2020-05-24 14:23:56 +0100

    Makes API endpoints internal

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,6 +1,24 @@
+/**
+ * @internal
+ */
 export const accessTokensRevoke = "access_tokens/revoke";
+/**
+ * @internal
+ */
 export const projects = "projects";
+/**
+ * @internal
+ */
 export const sections = "sections";
+/**
+ * @internal
+ */
 export const tasks = "tasks";
+/**
+ * @internal
+ */
 export const comments = "comments";
+/**
+ * @internal
+ */
 export const labels = "labels";
