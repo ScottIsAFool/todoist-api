39c4a331a43dd2940cab65d7a1d4dec1070a7f3f
Scott Lovegrove <scottisafool@live.co.uk>
2020-07-12 19:12:07 +0100

    Changes package config

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as TodoistClient from "./todoistClient";
+
+export default TodoistClient;
+
+export * from "./colors";
+export * from "./entities";
+export * from "./scopes";
+export * from "./webhookEntities";
+export * from "./webhookTypes";
